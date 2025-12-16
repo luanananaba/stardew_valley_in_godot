fa87bf0a9fd10bb6e7560f9cbd1d4bb3051e5d0a
Luana <itsmeluanatatiana@gmail.com>
2025-12-16 10:36:16 -0300

    animationtree and player movement animation

--- scenes/characters/player.gd
+++ scenes/characters/player.gd
@@ -7,6 +7,7 @@ var speed := 50
 
 func _physics_process(_delta : float) -> void:
 	_move()
+	_animate()
 
 
 func _move() -> void:
@@ -14,6 +15,25 @@ func _move() -> void:
 	velocity = direction * speed
 	
 	move_and_slide()
+
+
+func _animate() -> void:
+	if direction:
+		var direction_animation = Vector2(round(direction.x), round(direction.y))
+		
+		$Animation/AnimationTree.set(
+			"parameters/MoveStateMachine/Walk/blend_position", direction_animation
+		)
+	
+	# My attempt to learn it.
+	#if direction == Vector2.RIGHT:
+		#$Animation/AnimationTree.set("parameters/MoveStateMachine/Walk/blend_position", Vector2.RIGHT)
+	#if direction == Vector2.LEFT:
+		#$Animation/AnimationTree.set("parameters/MoveStateMachine/Walk/blend_position", Vector2.LEFT)
+	#if direction == Vector2.UP:
+		#$Animation/AnimationTree.set("parameters/MoveStateMachine/Walk/blend_position", Vector2.UP)
+	#if direction == Vector2.DOWN:
+		#$Animation/AnimationTree.set("parameters/MoveStateMachine/Walk/blend_position", Vector2.DOWN)
 
 
 func tool_use_emit():
